c1c43eeac7ad797fad3800c411bd363dee9fe9cc
cozy code <cozy.code@outlook.com>
2015-02-27 16:54:48 +0900

    change to call WebAPI

--- AGSample/Scripts/app/todo.js
+++ AGSample/Scripts/app/todo.js
@@ -4,27 +4,50 @@
 var todoApp = angular.module('todoApp', ['ngResource']);
 
 todoApp.controller("TodoCtrl", function ($scope, $resource) {
-    var todoClass = $resource('/api/Todoes/:id', { id: '@Id' });
+    var todoClass = $resource('/api/Todoes/:id', { id: '@Id' }, { 'update': { method: 'PUT' } });
     $scope.todos = todoClass.query();
 
+    // set watch listener for each items to Update 
+    var deWatchTodo = new Array();
+    $scope.$watchCollection("todos", function (newVal) {
+        angular.forEach(deWatchTodo, function (value, key) { value(); });   //cleaer watch
+        deWatchTodo = [];
+        // set watcher each item
+        angular.forEach(newVal, function (value, key) {
+            var deregistration = $scope.$watch("todos[" + key + "]", function (newVal, oldVal, scope) {
+                if (JSON.stringify(newVal) !== JSON.stringify(oldVal)) {
+                    todoClass.update(newVal.Id, newVal);
+                }
+            }, true);
+            deWatchTodo.push(deregistration);
+        });
+    });
+    
     $scope.addTodo = function () {
-        $scope.todos.push({ text: $scope.todoText, done: false });
+        todoClass.save({ Text: $scope.todoText, Done: false }, function (result) {
+            $scope.todos.push(result);
+        });
         $scope.todoText = '';
     };
 
     $scope.remaining = function () {
         var count = 0;
         angular.forEach($scope.todos, function (todo) {
-            count += todo.done ? 0 : 1;
+            count += todo.Done ? 0 : 1;
         });
         return count;
     };
 
+    // delete done items
     $scope.archive = function () {
         var oldTodos = $scope.todos;
         $scope.todos = [];
-        angular.forEach(oldTodos, function (todo) {
-            if (!todo.done) $scope.todos.push(todo);
+        angular.forEach(oldTodos, function (value, key) {
+            if (value.Done) {
+                todoClass.delete(value.Id);
+            } else {
+                $scope.todos.push(value)
+            }
         });
     };
 });
