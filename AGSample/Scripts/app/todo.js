c866375ac41200b73fc08bd125cef2006ae64afe
cozy code <cozy.code@outlook.com>
2015-03-02 10:49:06 +0900

    comment by Japanese

--- AGSample/Scripts/app/todo.js
+++ AGSample/Scripts/app/todo.js
@@ -4,32 +4,41 @@
 var todoApp = angular.module('todoApp', ['ngResource']);
 
 todoApp.controller("TodoCtrl", function ($scope, $resource) {
-    var todoClass = $resource('/api/Todoes/:id', { id: '@Id' }, { 'update': { method: 'PUT' } });
+    //WebAPI呼び出し用オブジェクト作成
+    var todoClass = $resource('/api/Todoes/:id', { id: '@Id' }
+                                , { 'update': { method: 'PUT' } }     //既定でPUT呼び出しが無いので定義
+                                );
+    //ToDoリスト初期化
     $scope.todos = todoClass.query();
 
-    // set watch listener for each items to Update 
-    var deWatchTodo = new Array();
+    // 各アイテムに変更監視ハンドラをセット
+    var deWatchTodo = new Array();  //ハンドラリセット用配列
     $scope.$watchCollection("todos", function (newVal) {
-        angular.forEach(deWatchTodo, function (value, key) { value(); });   //cleaer watch
+        //古いハンドラをリセット
+        angular.forEach(deWatchTodo, function (value, key) { value(); });   
         deWatchTodo = [];
-        // set watcher each item
+        // 監視関数をセット
         angular.forEach(newVal, function (value, key) {
             var deregistration = $scope.$watch("todos[" + key + "]", function (newVal, oldVal, scope) {
-                if (JSON.stringify(newVal) !== JSON.stringify(oldVal)) {
+                if (JSON.stringify(newVal) !== JSON.stringify(oldVal)) {    //内容が変わっている場合だけWebAPIコール
                     todoClass.update(newVal.Id, newVal);
                 }
             }, true);
-            deWatchTodo.push(deregistration);
+            deWatchTodo.push(deregistration);   //リセット用に記録
         });
     });
     
+    // アイテム追加
     $scope.addTodo = function () {
+        //WebAPIコール
         todoClass.save({ Text: $scope.todoText, Done: false }, function (result) {
+            //成功したらクライアント側でも記録
             $scope.todos.push(result);
         });
         $scope.todoText = '';
     };
 
+    // 残り件数
     $scope.remaining = function () {
         var count = 0;
         angular.forEach($scope.todos, function (todo) {
@@ -38,15 +47,15 @@ todoApp.controller("TodoCtrl", function ($scope, $resource) {
         return count;
     };
 
-    // delete done items
+    // 終了済みアイテムを削除
     $scope.archive = function () {
         var oldTodos = $scope.todos;
         $scope.todos = [];
         angular.forEach(oldTodos, function (value, key) {
-            if (value.Done) {
-                todoClass.delete(value.Id);
-            } else {
-                $scope.todos.push(value)
+            if (value.Done) {               //終わってら
+                todoClass.delete(value.Id); //削除WebAPIコール
+            } else {                        //終わってなかったら
+                $scope.todos.push(value)    //ToDoリストに戻す
             }
         });
     };
